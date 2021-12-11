6a9568a3ef123b49240421e5b0f7ad2756c52392
easonyu0203 <62385417+easonyu0203@users.noreply.github.com>
2021-12-11 13:31:56 +0800

    mac done

--- Assets/Scripts/MoveMentController.cs
+++ Assets/Scripts/MoveMentController.cs
@@ -12,12 +12,17 @@ namespace ThirdPersonShooter
         public float JumpHeight;
         public float GravityScale;
 
+        [Header("Reference")]
+        [SerializeField] Transform VCamTarget;
+
         /// <summary>
         /// move character toward the direction relative to character's local cordinate
         /// </summary>
         public void Move(Vector2 direction)
         {
-
+            // get world direction
+            Vector3 worldDirection;
+            VCamTarget.forward
         }
 
         /// <summary>
